77bf45904655a80ea3c7664793d14222208e5e0a
korshchamp <mama_ama2@mail.ru>
2022-03-27 16:45:33 +0400

    first comm

---
+++ blog/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Order, StatusCrm, ComentCrm
+
+
+
+admin.site.register(Order)
+admin.site.register(StatusCrm)
+admin.site.register(ComentCrm)
+
+
---
+++ blog/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+
+
+class StatusCrm(models.Model):
+	Status_name = models.CharField(max_length = 200, verbose_name = 'Название статуса')
+
+	def __str__(self):
+		return self.Status_name
+
+	class Meta:
+		verbose_name = 'Статус'
+		verbose_name_plural = 'Статусы'
+
+
+class Order(models.Model):
+	order_dt = models.DateTimeField(auto_now = True)
+	order_name = models.CharField(max_length = 200)
+	order_phone = models.CharField(max_length = 200)
+	order_status = models.ForeignKey(StatusCrm, on_delete = models.PROTECT, null=True, blank = True, verbose_name = 'Статус')
+
+	def __str__(self):
+		return self.order_name
+
+	class Meta:
+		verbose_name = 'Заказ'
+		verbose_name_plural = 'Заказы'
+
+class ComentCrm(models.Model):
+	coment_binding = models.ForeignKey(Order, on_delete = models.CASCADE, verbose_name = 'Заявка')
+	coment_text = models.TextField(verbose_name = 'Текст комментария')
+	coment_date = models.DateTimeField(auto_now = True, verbose_name = 'Дата создания')
+
+	def __str__(self):
+		return self.coment_text
+
+	class Meta:
+		verbose_name = 'комментарий'
+		verbose_name_plural = 'комментарии'
